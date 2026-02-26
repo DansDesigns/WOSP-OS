a079adfddb9174caef034c81fe1946fc0c7b51d1
DanDan <dandan7932@hotmail.com>
2026-02-26 16:47:39 +0000

    Update install-update.sh

--- install-update.sh
+++ install-update.sh
@@ -25,28 +25,32 @@ cd "$ALT_ROOT/wosp-shell" || { echo "ERROR: $ALT_ROOT not found"; exit 1; }
 # Update the Updater:
 #===========================================================
 echo "• Updating WOSP-OS Updater..."
+
 #=========================================
 # Un-Comment to update icon:
-#sudo cp icons/os-check-update.png /usr/share/icons/hicolor/64x64/apps/os-check-update.png
+sudo cp icons/os-check-update.png /usr/share/icons/hicolor/64x64/apps/os-check-update.png
+
 #=========================================
 # Update Updater:
 chmod +x $HOME/WOSP-OS/update/os-check-update
 sudo cp $HOME/WOSP-OS/update/os-check-update /usr/bin/
+
 #=========================================
 # Update Version Number:
 sudo cp $HOME/WOSP-OS/update/version.txt /usr/share/wosp/
+
 #=========================================
 # Un-Comment to Update the App Launcher:
 
-#sudo tee /usr/share/applications/os-check-update.desktop >/dev/null <<EOF
-#[Desktop Entry]
-#Name=System Update
-#Exec=alacritty -e /usr/bin/os-check-update
-#Icon=os-check-update
-#Type=Application
-#Terminal=true
-#Categories=System;
-#EOF
+sudo tee /usr/share/applications/os-check-update.desktop >/dev/null <<EOF
+[Desktop Entry]
+Name=System Update
+Exec=alacritty -e /usr/bin/os-check-update
+Icon=os-check-update
+Type=Application
+Terminal=true
+Categories=System;
+EOF
 
 cd /
 sudo rm -rf "$ALT_ROOT"
@@ -55,5 +59,5 @@ echo " "
 echo " "
 echo " "
 echo "=============================================="
-echo "     WOSP-OS Update Complete!"
+echo "         WOSP-OS Update Complete!"
 echo "=============================================="
